9dffe13558456877e6b1df0ca6bcae4d3fcee5b6
Matthew Deagen <36462122+mattdeagen@users.noreply.github.com>
2022-05-03 19:38:35 -0700

    Polish UI and Add Game Logic

--- script.js
+++ script.js
@@ -1,34 +1,90 @@
-
+// Choices array
 const choices = ['rock', 'paper', 'scissors']
 
+// Get buttons
+const rockBtn = document.getElementById('rock-btn');
+const paperBtn = document.getElementById('paper-btn');
+const scissorsBtn = document.getElementById('scissors-btn');
+
+// Get results paragraph
+const resultsPara = document.getElementById('results');
+
+// Get scores
+const playerScore = document.getElementById('player-score');
+const computerScore = document.getElementById('computer-score');
 
 // Randomly return rock, paper, or scissors
 function computerPlay() {
     return choices[Math.floor(Math.random()*choices.length)]
 }
 
-// Play a single round
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
-    // Player wins
-    // rock --- scissors
-    // paper --- rock
-    // scissors --- paper
-    if(playerSelection === computerSelection) {
-        return 'It\'s a tie!'
+// Render result to screen
+function renderResult(message) {
+    resultsPara.textContent = message
+    if(playerScore.textContent == '5' || computerScore.textContent == '5'){
+        if(playerScore.textContent == '5') {
+            renderGameOver('You')
+        } else {
+            renderGameOver('The computer')
+        }
+        
     }
-    else if(playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper'){
-        return `You Win! ${playerSelection} beats ${computerSelection}`
-    }
-    else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`
-    }
-    
 }
 
-// const playerSelection = window.prompt('Enter you choice.')
-const computerSelection = computerPlay()
+// Render game over and reset button
+function renderGameOver(winner) {
 
-// console.log(`Player selected ${playerSelection}\nComputer selected ${computerSelection}`);
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
 
-// console.log(playRound(playerSelection, computerSelection));
+    const gameOver = document.createElement('p')
+    gameOver.setAttribute('id', 'gameOverText')
+    const resetGame = document.createElement('button')
+    resetGame.setAttribute('id', 'resetGameBtn')
+
+    gameOver.textContent = `Game Over! ${winner} won!`
+    resetGame.textContent = "New Game?"
+    resetGame.addEventListener('click', resetScores)
+    resultsPara.append(gameOver, resetGame)
+}
+
+// Reset scores
+function resetScores() {
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+    
+    playerScore.textContent = '0';
+    computerScore.textContent = '0';
+    resultsPara.removeChild(document.getElementById('gameOverText'));
+    resultsPara.removeChild(document.getElementById('resetGameBtn'));
+    resultsPara.textContent = '';
+    // document.getElementById('gameOverText').remove()
+    // document.getElementById('resetGameBtn').remove()
+}
+
+const playMatch = (playerSelection) => {
+    // Generate computer Selection
+    const computerSelection = computerPlay();
+    let message = '';
+    
+    // Determine who wins
+    if(playerSelection === computerSelection) {
+        message = 'It\'s a tie!';
+    }
+    else if(playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper'){
+        message = `You Win! ${playerSelection} beats ${computerSelection}`;
+        playerScore.textContent = parseInt(playerScore.textContent) + 1
+    }
+    else {
+        message = `You lose! ${computerSelection} beats ${playerSelection}`;
+        computerScore.textContent = parseInt(computerScore.textContent) + 1
+    }
+    // Render result
+    renderResult(message);
+}
+
+rockBtn.addEventListener('click', () => playMatch('rock'));
+paperBtn.addEventListener('click', () => playMatch('paper'));
+scissorsBtn.addEventListener('click', () => playMatch('scissors'));
